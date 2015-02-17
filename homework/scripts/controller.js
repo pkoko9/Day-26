8b2bcaa21c6d72ed5eaf0f2bae99df5a1d547633
Paul Kokoszyna <pkoko9@hotmail.com>
2015-02-17 10:56:45 -0600

    Question 4

--- homework/scripts/controller.js
+++ homework/scripts/controller.js
@@ -19,25 +19,38 @@
 
 // Question 2
 
+// app.controller('LoadingButtonCtrl', function($scope) {
+
+// 	$scope.showContentRed = function() {
+// 		// $scope.isContentVisible = !$scope.isContentVisible;
+// 		$scope.red = true;
+// 		$scope.green = false;
+// 		$scope.blue = false;
+// 	}
+// 	$scope.showContentGreen = function() {	
+// 		$scope.red = false;
+// 		$scope.green = true;
+// 		$scope.blue = false;
+// 	}
+// 	$scope.showContentBlue = function() {
+// 		$scope.red = false;
+// 		$scope.green = false;
+// 		$scope.blue = true;
+// 	}
+// });
+
+// Question 4
+
 app.controller('LoadingButtonCtrl', function($scope) {
 
-	$scope.showContentRed = function() {
-		// $scope.isContentVisible = !$scope.isContentVisible;
-		$scope.red = true;
-		$scope.green = false;
-		$scope.blue = false;
+	$scope.numLikes=0
+	$scope.addLike = function () {
+		$scope.numLikes ++
 	}
-	$scope.showContentGreen = function() {	
-		$scope.red = false;
-		$scope.green = true;
-		$scope.blue = false;
-	}
-	$scope.showContentBlue = function() {
-		$scope.red = false;
-		$scope.green = false;
-		$scope.blue = true;
-	}
+
 });
+
+
 
 
 
