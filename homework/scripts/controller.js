efc1ce9544c7a145266e099b39500e2fff57eefc
Paul Kokoszyna <pkoko9@hotmail.com>
2015-02-16 19:11:06 -0600

    Assignment 2

--- homework/scripts/controller.js
+++ homework/scripts/controller.js
@@ -1,47 +1,51 @@
 // Question 1
-app.controller('LoadingButtonCtrl', function($scope, $timeout) {
-	$scope.buttonText='Test';
-	$scope.buttonDisabled=false;
-	$scope.startLoading=function(){
-		console.log('Loading');
-		$scope.buttonText="Loading...";
-		$scope.buttonDisabled=true;
-		$timeout(
-			function() {
-				$scope.buttonText='Test';
-				$scope.buttonDisabled=false;
-			},
+// app.controller('LoadingButtonCtrl', function($scope, $timeout) {
+// 	$scope.buttonText='Test';
+// 	$scope.buttonDisabled=false;
+// 	$scope.startLoading=function(){
+// 		console.log('Loading');
+// 		$scope.buttonText="Loading...";
+// 		$scope.buttonDisabled=true;
+// 		$timeout(
+// 			function() {
+// 				$scope.buttonText='Test';
+// 				$scope.buttonDisabled=false;
+// 			},
 			
-			4000
-		);
-	} 
+// 			4000
+// 		);
+// 	} 
+// });
+
+// Question 2
+
+app.controller('LoadingButtonCtrl', function($scope) {
+
+	$scope.showContentRed = function() {
+		// $scope.isContentVisible = !$scope.isContentVisible;
+		$scope.red = true;
+		$scope.green = false;
+		$scope.blue = false;
+	}
+	$scope.showContentGreen = function() {	
+		$scope.red = false;
+		$scope.green = true;
+		$scope.blue = false;
+	}
+	$scope.showContentBlue = function() {
+		$scope.red = false;
+		$scope.green = false;
+		$scope.blue = true;
+	}
 });
 
 
-	// $window.location = 'http://google.com';
-	// $scope.submit = function(user) {
-	// 	console.log(user);
-	// };
-	// $scope.name = 'Aaron';
-	// $scope.isContentVisible = false;
-	// $scope.logName = function() {
-	// 	console.log($scope.name);
-	// };
 
-	// $scope.showContent = function() {
-	// 	$scope.isContentVisible = !$scope.isContentVisible;
-	// }
 
-	// $scope.buttonText = 'Click Me';
-	// $scope.buttonDisabled = false;
-	// $scope.startLoading = function() {
-	// 	$scope.buttonText = 'Loading...';
-	// 	$scope.buttonDisabled = true;
 
-	// 	setTimeout(function() {
-	// 		console.log('test');
-	// 		$scope.buttonText = 'Click Me';
-	// 		$scope.buttonDisabled = false;
-	// 		$scope.$apply();
-	// 	}, 2000);
-	// }
+
+
+
+
+
+
