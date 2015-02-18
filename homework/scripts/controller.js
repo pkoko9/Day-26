5999fa60b34ebef56b299086888c025c9cc016f3
Paul Kokoszyna <pkoko9@hotmail.com>
2015-02-18 11:25:21 -0600

    correct css error question 2

--- homework/scripts/controller.js
+++ homework/scripts/controller.js
@@ -19,25 +19,25 @@
 
 // Question 2
 
-// app.controller('LoadingButtonCtrl', function($scope) {
+app.controller('LoadingButtonCtrl', function($scope) {
 
-// 	$scope.showContentRed = function() {
-// 		// $scope.isContentVisible = !$scope.isContentVisible;
-// 		$scope.red = true;
-// 		$scope.green = false;
-// 		$scope.blue = false;
-// 	}
-// 	$scope.showContentGreen = function() {	
-// 		$scope.red = false;
-// 		$scope.green = true;
-// 		$scope.blue = false;
-// 	}
-// 	$scope.showContentBlue = function() {
-// 		$scope.red = false;
-// 		$scope.green = false;
-// 		$scope.blue = true;
-// 	}
-// });
+	$scope.showContentRed = function() {
+		// $scope.isContentVisible = !$scope.isContentVisible;
+		$scope.red = true;
+		$scope.green = false;
+		$scope.blue = false;
+	}
+	$scope.showContentGreen = function() {	
+		$scope.red = false;
+		$scope.green = true;
+		$scope.blue = false;
+	}
+	$scope.showContentBlue = function() {
+		$scope.red = false;
+		$scope.green = false;
+		$scope.blue = true;
+	}
+});
 
 // Question 4
 
@@ -52,23 +52,23 @@
 
 // Question 5
 
-app.controller('LoadingButtonCtrl', function($scope, $window) {
+// app.controller('LoadingButtonCtrl', function($scope, $window) {
 
-	// $window.location = 'http://theironyard.com';
-	// $scope.submit = function(user) {
-	// 	console.log(user);
-	// };
-	$scope.userName = 'Paul';
-	$scope.isContentVisible = false;
-	$scope.logName = function() {
-		console.log($scope.name);
-	};
+// 	// $window.location = 'http://theironyard.com';
+// 	// $scope.submit = function(user) {
+// 	// 	console.log(user);
+// 	// };
+// 	$scope.userName = 'Paul';
+// 	$scope.isContentVisible = false;
+// 	$scope.logName = function() {
+// 		console.log($scope.name);
+// 	};
 
-	$scope.showContent = function() {
-		$scope.isContentVisible = !$scope.isContentVisible;
-	}
+// 	$scope.showContent = function() {
+// 		$scope.isContentVisible = !$scope.isContentVisible;
+// 	}
 
-});
+// });
 
 
 
